c55a84136e75377d45914f908aa035e184e23742
SteveWillowby <justus.i.hibshman@gmail.com>
2018-11-19 19:47:22 -0500

    Driver has proper ccs

--- examples/svm/driver.py
+++ examples/svm/driver.py
@@ -43,7 +43,7 @@ if __name__ == '__main__':
 
     # Set up the SHADHO driver like usual
     opt = Shadho('bash svm_task.sh', space, timeout=3600)
-    opt.config.workqueue.name = 'jhibshma_010'
+    opt.config.workqueue.name = 'jhibshma_test_02'
     opt.config.workqueue.port = 0
 
     # Add the task files to the optimizer
@@ -57,9 +57,8 @@ if __name__ == '__main__':
     # search.
     # For example, in a cluster with 20 16-core, 25 8-core, and 50 4-core
     # nodes, we can specify:
-    opt.add_compute_class('16-core', 'cores', 16, max_tasks=10)
-    opt.add_compute_class('8-core', 'cores', 8, max_tasks=10)
-    opt.add_compute_class('4-core', 'cores', 4, max_tasks=10)
-    opt.add_compute_class('2-core', 'cores', 2, max_tasks=10)
+    opt.add_compute_class('4-core', 'cores', 4, max_tasks=30)
+    opt.add_compute_class('3-core', 'cores', 3, max_tasks=30)
+    opt.add_compute_class('2-core', 'cores', 2, max_tasks=30)
 
     opt.run()
