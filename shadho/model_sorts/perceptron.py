debugged things and added print outs. added more .astype(floats) in hopes it will fix the handle_ouput always outputing 0s or 1s. Also added to the model to use the sample for prediction if an average_params for that model does not exist.

shadho/model_sorts/perceptron.py
@@ -92,10 +92,12 @@ class Perceptron(object):
                     self.pred_queue[pred_cc]['looped'] = False
 
     def model_id_to_onehot(self, model_id):
-        return (self.model_ids == model_id).astype(int)
+        return (self.model_ids == model_id).astype(float)
+        #return (self.model_ids == model_id).astype(int)
 
     def compute_class_to_onehot(self, compute_class):
-        return (self.compute_class_ids == compute_class).astype(int)
+        return (self.compute_class_ids == compute_class).astype(float)
+        #return (self.compute_class_ids == compute_class).astype(int)
 
     def set_normalize_factors(self, shadho_backend, resources, weights=None):
         """
@@ -128,14 +130,15 @@ class Perceptron(object):
             normalize_factors = self.normalize_factors
 
         # assumes that there are more features than the 1 hot vectors
-        if self.input_length <= one_hot_size:
-            return input_vector
-            #elif len(input_vector) != self.input_length: # should throw error
-            #    return input_vector
-        else:
-            input_vector[:, one_hot_size:] = input_vector[:, one_hot_size:] / normalize_factors
+        #if self.input_length <= one_hot_size:
+        #    return input_vector
+        #    #elif len(input_vector) != self.input_length: # should throw error
+        #    #    return input_vector
+        #else:
+        input_vector = input_vector.astype(float)
+        input_vector[:, one_hot_size:] = input_vector[:, one_hot_size:] / normalize_factors
 
-        return input_vector
+        return input_vector.
 
     def handle_input(self, raw_input_vectors):
         """Handles multiple raw input vectors."""
@@ -145,7 +148,7 @@ class Perceptron(object):
                 input_vectors.append(self.param_averages[i[0]])
                 print('self.param_averages[i[0]]', self.param_averages[i[0]])
             else:
-                input_vector = np.append(np.append(self.model_id_to_onehot(i[0]), self.compute_class_to_onehot(i[1])),  i[2:]).reshape([1, -1])
+                input_vector = np.append(np.append(self.model_id_to_onehot(i[0]), self.compute_class_to_onehot(i[1])),  i[2:]).reshape([1, -1]).astype(float)
                 input_vectors.append(self.normalize_input(input_vector))
                 # TODO possibly add custom error for caller to handle.
         return input_vectors
@@ -290,17 +293,25 @@ class Perceptron(object):
             #print(f"rl_vector: {rl_vector} | post_losses: {self.sess.run(tf.get_collection('losses'), feed_dict={self.reinforcement_penalties: rl_vector, self.network_input: input_vector})}")
 
     def predict(self, input_vectors):
-        models = [x[0] for x in input_vectors]
+        print('predict: input vec:')
+        for input_vec in input_vectors:
+            print(input_vec)
 
-        # NOTE using the averages of the input_vectors instead.
-        #print(input_vectors[0][0])
-        #input_vectors = self.handle_input(input_vectors)
-        #print(input_vectors[0][0])
         logit_list = []
-        #for input_vector in input_vectors:
-        for model_id in models:
-            logit_list.append(self.sess.run(self.softmax_linear, feed_dict = {self.network_input : self.param_averages[model_id]}))
-            #logit_list.append(self.sess.run(self.softmax_linear, feed_dict = {self.network_input : input_vector}))
+        if self.param_averages[model_id] is not None:
+            # use the averages if exists
+            models = [x[0] for x in input_vectors]
+            for model_id in models:
+                    logit_list.append(self.sess.run(self.softmax_linear, feed_dict = {self.network_input : self.param_averages[model_id]}))
+        else:
+            # use the actual sample
+            input_vectors = self.handle_input(input_vectors)
+            print('predict handled input vec:')
+            for input_vec in input_vectors:
+                print(input_vec)
+
+            for input_vector in input_vectors:
+                logit_list.append(self.sess.run(self.softmax_linear, feed_dict = {self.network_input : input_vector}))
         # outputs log probabilities, convert to non-log probabilities
         logit_list = [np.e ** logits / np.sum(np.e**logits) for logits in logit_list]
         logit_list = [np.squeeze(logits) for logits in logit_list]

shadho/shadho.py
@@ -493,7 +493,7 @@ class Shadho(object):
             # if initial run of Perceptron, set normals: defaults to np.ones()
             if self.perceptron.normalize_factors is None:
                 #self.perceptron.set_normalize_factors(self.backend, self.feature_resources)
-                self.perceptron.set_normalize_factors(self.backend, self.feature_resources, [16.0, 16.0, 16000.0, 12000.0, 50000.0]) # hardcoded maxes, uncertain w/ setup atm.
+                self.perceptron.set_normalize_factors(self.backend, self.feature_resources, [16.0, 16000.0, 16.0, 12000.0, 50000.0]) # hardcoded maxes, uncertain w/ setup atm.
 
             # pull the recent models and turn into sample input + runtimes
             input_vectors = []
@@ -509,20 +509,41 @@ class Shadho(object):
                     if resources.loss is None or resources.results is None:
                         missing_input_vectors.append([model_id, None])
                     else:
-                        input_vectors.append([model_id, resources.results['compute_class']['name']] + [resources.results['resources_measured'][resrc] for resrc in self.feature_resources])
+                        input_vectors.append([model_id, resources.results['compute_class']['id']] + [resources.results['resources_measured'][resrc] for resrc in self.feature_resources])
                         runtimes.append(resources.results['finish_time'] - resources.results['start_time'])
+
+            print('perceptron pred queue:')
+            for key, value in self.perceptron.pred_queue.items():
+                print(key, ':', value)
 
             if input_vectors:
                 self.perceptron.update(input_vectors, runtimes)
+
+                #print('perceptron param_averages:')
+                #for key, value in self.perceptron.param_averages.items():
+                #    print(key, ':', value)
+
                 self.perceptron.predict(input_vectors) # updates pred_queue
                 no_input = False
                 if self.initial_generation:
                     self.initial_generation = False
                     print('Inital update to perceptron has occured!')
+
+                print('After update: perceptron pred queue:')
+                for key, value in self.perceptron.pred_queue.items():
+                    print(key, ':', value)
             else:
                 no_input = True
             if missing_input_vectors and not self.perceptron.any_pred_queue_empty:
+                print('missing input vectors used in prediction')
+                for missing in missing_input_vectors:
+                    print(missing)
+
                 self.perceptron.predict(missing_input_vectors) # updates pred_queue
+
+                print('After update: perceptron pred queue:')
+                for key, value in self.perceptron.pred_queue.items():
+                    print(key, ':', value)
             #elif no_input:
             #    if self.initial_generation: # if never updated, assign all again.
             #        for key in list(self.ccs.keys()):